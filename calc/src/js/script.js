bf829d3a26504e21bab7e3ecdb252795b12411fb
Антон <89977334+raazuev@users.noreply.github.com>
2024-07-21 15:35:20 +0500

    Add files via upload

---
+++ calc/src/js/script.js
@@ -0,0 +1,27 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    if (display.value !== '') {
+        display.value += operator;
+    }
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Ошибка';
+    }
+}
